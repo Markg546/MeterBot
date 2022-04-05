bc956155b9e5292c1b8f859a0f95c6f5162441ab
Mark <markg546@gmail.com>
2022-04-05 14:47:01 +0200

    Update ocr.py

--- OCRBot/ocr.py
+++ OCRBot/ocr.py
@@ -4,9 +4,12 @@ from PIL import Image
 from pyrogram.types import Message
 from pyrogram import Client, filters
 from pyrogram.errors import MessageEmpty
+import cv2
+import numpy as np
 
 # pytesseract.pytesseract.tesseract_cmd = r""
 
+custom_config = r'-c tessedit_char_whitelist=0123456789- --psm 12'
 
 @Client.on_message(filters.private & filters.incoming & filters.photo)
 async def _ocr(_, msg: Message):
@@ -16,7 +19,15 @@ async def _ocr(_, msg: Message):
     message = await msg.reply("Asteptati...")
     image = await msg.download(file_name=f"{name_format}.jpg")
     img = Image.open(image)
-    text = pytesseract.image_to_string(img, config='outputbase digits')
+    
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # pre-processing starts here
+    gray = cv2.fastNlMeansDenoising(gray, None, 10, 7, 21)
+    gray = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
+    gray = cv2.erode(gray, np.ones((3,2),np.uint8), iterations = 1)
+    
+    text = pytesseract.image_to_string(gray, config=custom_config)
+    
     text = text[:-1]
     try:
         await msg.reply(text, quote=True, disable_web_page_preview=True)
