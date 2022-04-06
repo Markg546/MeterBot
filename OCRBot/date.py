60612f3b5bb293026c9b32520a1f7024f6c8d5c8
Mark <markg546@gmail.com>
2022-04-06 15:14:58 +0200

    update

--- OCRBot/date.py
+++ OCRBot/date.py
@@ -6,11 +6,11 @@ from pyrogram.types import InlineKeyboardMarkup
 # Help Message
 @Client.on_message(filters.private & filters.incoming & filters.command("date"))
 async def _help(bot, msg):
+    bot.delete_messages(chat_id, message_id)
     await bot.send_photo(
         msg.chat.id,
         "https://i.imgur.com/WGbwhqH.jpg",
         caption="00637",
-        #reply_markup=InlineKeyboardMarkup(Data.home_buttons)
+        reply_markup=InlineKeyboardMarkup(Data.home_buttons)
     )
-    await message.delete()
 
